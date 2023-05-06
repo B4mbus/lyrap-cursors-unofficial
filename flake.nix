d0edce28f526bf4c7a3ea3885ac67b44ada30327
Daniel Zaradny <danielzaradny@gmail.com>
2023-05-06 06:43:03 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "LyraP Cursors";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      formatter = pkgs.alejandra;
+
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "lyrap-cursors";
+        pname = "lyrap-cursors";
+        src = self;
+
+        installPhase = let
+          installPath = "$out/share/icons/LyraP-Cursors";
+        in ''
+          mkdir -p ${installPath}
+          cp -va index.theme cursors ${installPath}
+        '';
+      };
+    });
+}
